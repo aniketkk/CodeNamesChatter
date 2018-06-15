e53e5de879601af3d1f6d169598e9bd65c3ad6cf
aniketkk <aniketrulz18@gmail.com>
2018-06-14 22:26:52 -0700

    fixes to chat server
    fixed chat issues

--- app.js
+++ app.js
@@ -32,26 +32,30 @@ io.on('connection', (socket) => {
             username: socket.username,
             message: data
         });
-        socket.broadcast.emit('new message', {
+        socket.to(socket.gameid).emit('new message', { //except sender
             username: socket.username,
             message: data
         });
     });
 
     // when the client emits 'add user', this listens and executes
-    socket.on('add user', (username) => {
-        console.log("added user");
+    socket.on('add user', (data) => {
+
         if (addedUser) return;
 
+        socket.gameid = data.gameId;
+        socket.join(socket.gameid);
+        console.log("added user here " + data.username + " joined "+ socket.gameid);
         // we store the username in the socket session for this client
-        socket.username = username;
+        socket.username = data.username;
         ++numUsers;
         addedUser = true;
-        socket.emit('login', {
+        io.in(socket.gameid).emit('login', {
             numUsers: numUsers
         });
+        console.log(numUsers);
         // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('user joined', {
+        io.in(socket.gameid).emit('user joined', {
             username: socket.username,
             numUsers: numUsers
         });
@@ -59,15 +63,22 @@ io.on('connection', (socket) => {
 
     // when the client emits 'typing', we broadcast it to others
     socket.on('typing', () => {
-        socket.broadcast.emit('typing', {
+        io.in(socket.gameid).emit('typing', {
             username: socket.username
         });
     });
 
     // when the client emits 'stop typing', we broadcast it to others
     socket.on('stop typing', () => {
+        console.log(socket.username+ " stopped typing");
+        socket.to(socket.gameid).emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    socket.on('selected cards', () => {
         console.log(socket.username);
-        socket.broadcast.emit('stop typing', {
+        io.in(socket.gameid).emit('stop typing', {
             username: socket.username
         });
     });
@@ -78,7 +89,7 @@ io.on('connection', (socket) => {
             --numUsers;
 
             // echo globally that this client has left
-            socket.broadcast.emit('user left', {
+            io.in(socket.gameid).emit('user left', {
                 username: socket.username,
                 numUsers: numUsers
             });
