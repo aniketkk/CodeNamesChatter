fd7b5a3397b3aeeb161094acaee44a099915f444
aniketkk <aniketrulz18@gmail.com>
2018-06-14 23:19:35 -0700

    removing for prod

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ server.listen(port, () => {
 
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
+/*
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "https://codenames-chat-server.herokuapp.com/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+});*/
 
 // Chatroom
 
